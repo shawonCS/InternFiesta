a14364d86ec70a409877f7b6a487e5c9abd8cb5b
shawonCS <shawonsamiuzzaman@gmail.com>
2026-09-25 23:09:39 +0600

    T-07.2 Add active internship retrieval for students

---
+++ InternFiesta/InternFiesta/Controllers/StudentInternshipsController.cs
@@ -0,0 +1,30 @@
+﻿using InternFiesta.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InternFiesta.Controllers
+{
+    [Authorize(Roles = "Student")]
+    public class StudentInternshipsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentInternshipsController(
+            ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var internships = await _context.InternshipPostings
+                .Where(i => i.IsActive)
+                .OrderByDescending(i => i.CreatedAt)
+                .ToListAsync();
+
+            return View(internships);
+        }
+    }
+}
